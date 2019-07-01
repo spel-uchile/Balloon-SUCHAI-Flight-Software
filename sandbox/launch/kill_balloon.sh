e16caad0ded8a4b11875b934468ca37ec4adc2da
gdiazh <g.hernan.diaz@gmail.com>
2019-07-01 17:29:31 -0400

    added nodes to launch

--- sandbox/launch/kill_balloon.sh
+++ sandbox/launch/kill_balloon.sh
@@ -4,9 +4,10 @@ bmp_id=$(ps aux | grep bmp | grep -v grep | awk '{print $2}')
 gps_id=$(ps aux | grep gps | grep -v grep | awk '{print $2}')
 ird_id=$(ps aux | grep ird | grep -v grep | awk '{print $2}')
 sstv_id=$(ps aux | grep sstv | grep -v grep | awk '{print $2}')
+xbee_id=$(ps aux | grep xbee | grep -v grep | awk '{print $2}')
 hub_id=$(ps aux | grep hub | grep -v grep | awk '{print $2}')
 suchai_id=$(ps aux | grep SUCHAI | grep -v grep | awk '{print $2}')
 direwolf_id=$(ps aux | grep direwolf | grep -v grep | awk '{print $2}')
 
-kill $dpl_id $bmp_id $gps_id $ird_id $hub_id $suchai_id $direwolf_id $sstv_id
+kill $dpl_id $bmp_id $gps_id $ird_id $hub_id $suchai_id $direwolf_id $sstv_id $xbee_id
 echo Killed balloon
